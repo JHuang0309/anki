999aab17a0cb12ff41828c9e58924fe6875ed562
Jayden Huang <jhuang.study@gmail.com>
2024-12-31 11:51:39 +1100

    Added server imports and packages

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,32 @@
-const express = require('express')
+/**************************************************************************************/
+/**********  SERVER FUNCIONTALITY FILE  **********************************************
+*      
+*      File to manage requests made to the server 
+*
+/*************************************************************************************/
+import fs from 'fs';
+import express from 'express';
+// import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+
+import { InputError, AccessError, } from './error';
+import {
+    save,
+    reset,
+    assertValidUserId,
+    assertAdminUserId,
+    getUserIdFromAuthorization,
+    getUserIdFromEmail,
+    login,
+    register,
+    userGet,
+    userAdminChange,
+    userUpdate,
+  } from './service';
+
 const app = express()
+
+app.use(cors()); //  allow any incoming HTTP requests from any origin to access the server's resources
 
 app.get("/api", (req, res) => {
     res.json({"users": ["jason", "jayden", "goran", "vahin"]})
