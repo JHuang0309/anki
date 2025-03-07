92367342d563da7091d683ef3bb6a6f1a43533de
Jayden Huang <jhuang.study@gmail.com>
2025-03-08 00:49:44 +1100

    Added shuffle and play feature

--- frontend/src/pages/PagePlay.jsx
+++ frontend/src/pages/PagePlay.jsx
@@ -14,7 +14,7 @@ const PagePlay = () => {
     const [desc, setDesc] = useState('');
     const [answer, setAnswer] = useState('');
     const [showAnswer, setShowAnswer] = useState(false)
-  
+    const [deckLen, setDeckLen] = useState(0)
 
     const fetchCards = () => {
       axios.get('http://localhost:5005/cards', {
@@ -25,6 +25,7 @@ const PagePlay = () => {
       })
       .then(res => {
         setCards(shuffleCards(res.data.cards));
+        setDeckLen(res.data.cards.length)
       })
       .catch(res => {
         console.error("Unexpected error:", res);
@@ -38,6 +39,21 @@ const PagePlay = () => {
         [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
       }
       return shuffledArray;
+  }
+
+  const handleNavCards = (direction) => {
+    const cur = viewCardIndex
+    if (direction == 'prev') {
+      if (cur > 0) {
+        setViewCardIndex(cur - 1);
+        setShowAnswer(false);
+      }
+    } else {
+      if (cur < deckLen) {
+        setViewCardIndex(cur + 1);
+        setShowAnswer(false);
+      }
+    }
   }
 
   // UseEffects
@@ -55,6 +71,15 @@ const PagePlay = () => {
     }
   }, [cards])
 
+  useEffect(() => {
+    const viewCard = cards[viewCardIndex]
+    if (viewCard) {
+      setTitle(viewCard.title)
+      setDesc(viewCard.desc)
+      setAnswer(viewCard.answer)
+    }
+  }, [viewCardIndex])
+
   const mainStyle= { margin: '3em', marginTop: '1em'};
 	return (
 		<>
@@ -65,11 +90,18 @@ const PagePlay = () => {
               onClick={() => { navigate(`/`) }}
             >Return</button>
             <h1 className='font-bold text-xl'>{deckTitle}</h1>
-            <div></div>
+            <button 
+              className='text-sm bg-blue-100 text-blue-700 py-1 px-2 rounded-md transition duration-200 hover:bg-blue-200'
+              onClick={() => {
+                setCards(shuffleCards(cards))
+                setViewCardIndex(0);
+                setShowAnswer(false);
+              }}
+            >Reshuffle</button>
         </div>
       <hr className="border-t border-gray-300 my-4"/>
           <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-4">
-              <div className="flex flex-col bg-white px-4 pb-4 pt-5 sm:py-6 sm:px-4 sm:pb-4">
+              <div className="flex min-h-[100%] min-w-[100%] flex-col bg-white px-4 pb-4 pt-5 sm:py-6 sm:px-4 sm:pb-4">
                 <div className="mt-3 text-center sm:mx-4 sm:mt-0 sm:text-left">
                     <h3 className="text-base font-semibold text-gray-900">
                         {title}
@@ -103,14 +135,19 @@ const PagePlay = () => {
                     <button
                       type="button"
                       data-autofocus
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      className={`mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto ${viewCardIndex === 0 ? 'cursor-not-allowed opacity-50' : ''}`}
+                      disabled={viewCardIndex === 0}
+                      onClick={() => handleNavCards('prev')}
                     >
                       Previous
                     </button>
+                    <p className='flex text-sm text-gray-400 items-center'>{viewCardIndex + 1}/{deckLen}</p>
                     <button
                       type="button"
                       data-autofocus
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      className={`mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto ${viewCardIndex === deckLen - 1 ? 'cursor-not-allowed opacity-50' : ''}`}
+                      disabled={viewCardIndex === deckLen - 1}
+                      onClick={() => handleNavCards('next')}
                     >
                       Next
                     </button>
