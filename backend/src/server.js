61f858c8146d9fd3897a5cbd0626053c27623a0a
Jayden Huang <jhuang.study@gmail.com>
2025-03-08 12:39:23 +1100

    Added the possibility of a public port

--- backend/src/server.js
+++ backend/src/server.js
@@ -226,7 +226,7 @@ app.get("/api", (req, res) => {
     res.json({"users": ["jason", "jayden", "goran", "vahin"]})
 })
 
-const PORT = 5005
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
