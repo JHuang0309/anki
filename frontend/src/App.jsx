7d3a5ecf66ece4eb7aa3760d7a8644845675f43e
Jayden Huang <jhuang.study@gmail.com>
2025-03-14 12:29:57 +1100

    Changing backend url to local

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,8 +22,8 @@ function App() {
   const [backend, setBackend] = useState([{}])
 
   useEffect(() => {
-    fetch("https://anki-ug7c.onrender.com/api").then(res => res.json()).then(data => setBackend(data))
-    // fetch("/api").then(res => res.json()).then(data => setBackend(data))
+    // fetch("https://anki-ug7c.onrender.com/api").then(res => res.json()).then(data => setBackend(data))
+    fetch("/api").then(res => res.json()).then(data => setBackend(data))
     // fetch("https://anki-production-601d.up.railway.app/api").then(res => res.json()).then(data => setBackend(data))
   }, [])
 
